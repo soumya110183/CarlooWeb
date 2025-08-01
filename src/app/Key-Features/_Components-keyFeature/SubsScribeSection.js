9b726d8e8d85cf7403eb8544f01e0384036dd35d
Anandhu Prasad <you@example.com>
2025-08-01 13:11:55 +0530

    key features

--- src/app/Key-Features/_Components-keyFeature/SubsScribeSection.js
+++ src/app/Key-Features/_Components-keyFeature/SubsScribeSection.js
@@ -1,5 +1,14 @@
 import HeadingReuse from "@/app/_subcomponents/HeadingReuse";
 import Image from "next/image";
+import {
+  FaTwitter,
+  FaFacebookF,
+  FaInstagram,
+  FaLinkedinIn,
+  FaGlobe,
+  FaLeaf,
+  FaCommentDots,
+} from "react-icons/fa";
 
 export default function SubScribeSection() {
   return (
@@ -16,7 +25,31 @@ export default function SubScribeSection() {
             and be a part of our community and get the latest news and insights
           </p>
         </div>
-        
+        <form className="flex w-full bg-black items-center p-3.5 rounded-[10px] justify-between mt-6">
+          <input
+            type="email"
+            placeholder="Enter your email"
+            className="px-4 py-2 w-full sm:w-2/3 rounded-full text-white border-none outline-none  placeholder:text-gray-500 focus:outline-none focus:ring-0"
+            required
+          />
+          <button
+            type="submit"
+            className="bg-[#651FFF] p-2.5 hover:bg-purple-600 text-white font-semibold  rounded-[10px] transition"
+          >
+            Subscribe
+          </button>
+          
+        </form>
+        <div className="flex gap-4 mt-4">
+            {[FaTwitter, FaFacebookF, FaInstagram, FaLinkedinIn].map(
+              (Icon, idx) => (
+                <Icon
+                  key={idx}
+                  className="text-white bg-black rounded-full p-2 w-8 h-8 cursor-pointer hover:scale-105 transition"
+                />
+              )
+            )}
+          </div>
       </div>
       <div>
         <Image
