ccb9dcdf2c5e503e6ce1d4e976366bb13917b5eb
Anandhu <anandhup059@gmail.com>
2025-08-08 06:39:41 +0530

    minor fixes

--- src/app/About-us/_components/TeamContainer.js
+++ src/app/About-us/_components/TeamContainer.js
@@ -1,13 +1,14 @@
-"use client"
+"use client";
 
 import { useTheme } from "@/app/_subcomponents/ThemeContext";
 import Image from "next/image";
 
 export default function TeamContainer({ img, name, role, details }) {
-
-  const {theme}=useTheme()
+  const { theme } = useTheme();
   return (
-    <div className={`bg-[#0F092A] px-10 items-center ${theme === "light" ? "text-white" : "text-black"}  justify-items-center rounded-3xl py-10 "`}>
+    <div
+      className={`bg-[#0F092A] px-10 items-center text-white justify-items-center rounded-3xl py-10 "`}
+    >
       <div>
         <div className="relative w-56 h-56 rounded-full border border-white overflow-hidden mx-auto">
           <Image
--- src/app/_components/Services.js
+++ src/app/_components/Services.js
@@ -1,19 +1,16 @@
-
-
+import Link from "next/link";
 import Percentage from "../_subcomponents/Percentage";
 import { useTheme } from "../_subcomponents/ThemeContext";
 
 export default function Services() {
-
-
-
-
   return (
     <section>
-      <div className={`w-full max-w-[1200px] flex mx-auto mt-36 justify-between text-foreground`}>
+      <div
+        className={`w-full max-w-[1200px] flex mx-auto mt-36 justify-between text-foreground`}
+      >
         <div className="w-full max-w-[434px]">
           <h2 className="text-[32px] font-bold">
-             Compliance Intelligence That Powers Results
+            Compliance Intelligence That Powers Results
           </h2>
           <div className="flex gap-2 mt-4">
             <div className="w-4 h-3 rounded-full bg-gradient-to-r from-pink-300 via-cyan-300 to-pink-200"></div>
@@ -21,7 +18,10 @@ export default function Services() {
             <div className="w-24 h-3 rounded-full bg-gradient-to-r from-pink-300 via-cyan-300 to-pink-200"></div>
           </div>
           <p className="w-full text-[18px] font-medium mt-4">
-Carlo PEaaS is not just a tool—it&apos;s your real-time compliance co-pilot. Whether you&apos;re developing AI for finance, healthcare, retail, or any regulated industry, Carlo ensures your models stay aligned with global laws and ethical principles from day one.
+            Carlo PEaaS is not just a tool—it&apos;s your real-time compliance
+            co-pilot. Whether you&apos;re developing AI for finance, healthcare,
+            retail, or any regulated industry, Carlo ensures your models stay
+            aligned with global laws and ethical principles from day one.
           </p>
         </div>
         <div className="w-full max-w-[570px]">
@@ -37,7 +37,7 @@ Carlo PEaaS is not just a tool—it&apos;s your real-time compliance co-pilot. W
             </div>
 
             <button className="text-white border-none  m-0  outline-none bg-[#651FFF]   p-2.5 text-[15px] rounded-[36px] font-semibold">
-              Contact Us
+              <Link href={"/Contact"}> Contact Us</Link>
             </button>
           </div>
         </div>
