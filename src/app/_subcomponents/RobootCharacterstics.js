22f046d46050d3d5525d76b881e62a257f9d3004
Anandhu Prasad <anandhup059@gmail.com>
2025-08-14 16:08:04 +0530

    rak-dao

--- src/app/_subcomponents/RobootCharacterstics.js
+++ src/app/_subcomponents/RobootCharacterstics.js
@@ -1,41 +1,40 @@
-
-
 import Image from "next/image";
-
+import { motion } from "framer-motion";
 
 function RobootCharacterstics({ img, title, paragraph, imp, flex }) {
-
- 
   return (
-    <div
+    <motion.div
+      initial={{ opacity: 0, x: flex ? 100 : -100 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      viewport={{ once: true, amount: 0.3 }}
+      transition={{ duration: 0.8, ease: "easeOut" }}
       className={`w-full max-w-[1000px] mx-auto flex ${
         flex ? "flex-row-reverse" : "flex"
-      } justify-between text-white items-center `}
+      } justify-between text-white items-center my-12`}
     >
-      <div >
+      <div>
         <Image
           src={img}
           alt="robot-validator"
           width={314}
           height={479}
-          className="w-[314px] h-auto" 
+          className="w-[314px] h-auto"
         />
       </div>
       <div className={`max-w-[601px] text-foreground`}>
         <h3 className={`text-[40px] font-bold w-full`}>{title}</h3>
         <p className="text-[22px] font-medium mt-3.5">{paragraph}</p>
-        <div className="flex gap-3 items-center">
+        <div className="flex gap-3 items-center mt-8">
           <Image
             src="/material-icon-theme_robots.png"
             alt="icon-robo"
             width={37}
             height={37}
-            className="mt-8"
           />
-          <p className="text-[18px] font-semibold mt-8">{imp}</p>
+          <p className="text-[18px] font-semibold">{imp}</p>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
 
