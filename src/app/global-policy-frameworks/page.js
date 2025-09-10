62901fd00e1966740c371b27551235982e3789c9
soumya110183 <mail2soumyavs@gmail.com>
2025-09-10 12:31:17 +0530

    Update page.js

--- src/app/global-policy-frameworks/page.js
+++ src/app/global-policy-frameworks/page.js
@@ -1,6 +1,13 @@
 import Ethics from "./_components/Ethics";
 import GlobalPolicy from "./_components/GlobalPolicy";
 
+
+export const metadata = {
+  title: "Global Compliance Frameworks",
+  description: "Carlo maps policies to GDPR, EU AI Act, HIPAA, ISO, and more. Automate reporting and ensure continuous AI compliance worldwide.",
+  keywords: "GDPR, EU AI Act, HIPAA, ISO, AI governance frameworks",
+};
+
 export default function page() {
   return <div className="mt-50 overflow-y-hidden px-10 max-sm:px-5">
     <GlobalPolicy />
