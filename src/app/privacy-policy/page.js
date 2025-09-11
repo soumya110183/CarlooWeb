504a679e9996e2365a43ce7c4c6a250eeef5dfa3
soumya110183 <mail2soumyavs@gmail.com>
2025-09-11 17:10:39 +0530

    Update page.js

--- src/app/privacy-policy/page.js
+++ src/app/privacy-policy/page.js
@@ -1,5 +1,12 @@
 import PrivacyPolicy from "./PrivacyPolicy";
 
+export const metadata = {
+  title: "Privacy Policy: Carlo PEaaS Data Protection and User Privacy",
+  description: "Read Carlo PEaaS Privacy Policy to understand how we collect, use, and protect your personal data. Learn about our compliance with global data protection laws like GDPR, CCPA, and how we ensure your information is secure.",
+  keywords: "Carlo PEaaS, privacy policy, data protection, GDPR compliance, CCPA compliance, user privacy, personal data security, information protection, data governance, ethical AI",
+};
+
+
 export default function page(){
     return(
        <section className={`w-full max-w-[1280px]  mx-auto flex flex-col lg:flex-row justify-between  gap-10 lg:pb-32 pb-15 text-foreground items-center mt-30`}>
