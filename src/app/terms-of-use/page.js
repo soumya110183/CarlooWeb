c5964561b53e3d938a5e046d3eb1a8e064282575
soumya110183 <mail2soumyavs@gmail.com>
2025-09-11 17:11:34 +0530

    Update page.js

--- src/app/terms-of-use/page.js
+++ src/app/terms-of-use/page.js
@@ -1,6 +1,13 @@
 
 import TermsOfUse from "./TermsofUSe";
 
+export const metadata = {
+  title: "Terms of Use: Carlo PEaaS Platform Rules and Guidelines",
+  description: "Review Carlo PEaaS Terms of Use to understand the rules, guidelines, and legal obligations when using our AI compliance and governance platform. Ensure proper and responsible use of our services.",
+  keywords: "Carlo PEaaS, terms of use, platform rules, user guidelines, legal obligations, responsible use, AI compliance platform, ethical AI usage",
+};
+
+
 export default function page(){
     return(
        <section className={`w-full max-w-[1280px]  mx-auto flex flex-col lg:flex-row justify-between  gap-10 lg:pb-32 pb-15 text-foreground items-center mt-30`}>
