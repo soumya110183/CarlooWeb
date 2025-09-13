102e4672eeaeb78727883fb1ef227be396a52ed1
soumya110183 <mail2soumyavs@gmail.com>
2025-09-13 14:26:59 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,12 +13,14 @@ const montserrat = Montserrat({
 });
 
 export const metadata = {
-  title: "Carlo PEaaS | Ethical and Data Science",
-  description: "Ethical and Data Science",
+  title: "Carlo PEaaS | Real-Time AI Compliance, Ethical Governance & Data Science Security",
+  description: "Ensure your AI stays ethical, secure, and compliant with global standards like GDPR, HIPAA & EU AI Act. Carlo PEaaS offers real-time monitoring, automated enforcement, and enterprise-scale governance for trustworthy AI.",
+  keywords: "AI compliance, ethical AI, data security, GDPR, HIPAA, EU AI Act, AI governance, AI monitoring, enterprise AI, AI enforcement, data science security",
   icons: {
     icon: "/logo black-carlo.png",
   },
 };
+
 
 export default function RootLayout({ children }) {
   return (
